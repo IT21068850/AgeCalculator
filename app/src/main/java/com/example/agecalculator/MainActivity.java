95993eca5427620a7d2da24a9ff80c8c0b3146a4
Yusri <it21068850@my.sliit.lk>
2022-08-16 12:34:22 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/agecalculator/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.agecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView tv;
+    Button btn;
+    EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et=findViewById(R.id.etYear);
+        tv = findViewById(R.id.tvDisplay);
+        btn = findViewById(R.id.btnCalculate);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                 Integer Birthyear =Integer.parseInt(et.getText().toString());//editable
+                 Integer CurrentYear = Calendar.getInstance().get(Calendar.YEAR);
+
+
+                 int age = CurrentYear - Birthyear;
+
+                 tv.setText("Your age is : " +age+ " years old");
+            }
+        });
+    }
+
+
+
+
+
+
+}
